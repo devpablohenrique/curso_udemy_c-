4b4c8af85c3e9c8364653d722fe604e7c86bac53
devpablohenrique <devpablohenrique@gmail.com>
2022-05-31 15:44:05 -0300

    Operadores aritiméticos

--- Course/Course/Program.cs
+++ Course/Course/Program.cs
@@ -8,12 +8,26 @@ namespace Course
         static void Main(string[] args)
         {
 
-            int a = 5;
-            int b = 2;
+            int n1 = 3 + 4 * 2;
+            int n2 = (3 + 4) * 2;
+            int n3 = 17 % 3;
+            double n4 = 10.0 / 8;
 
-            double resulta = (double) a / b;
+            double a = 1.0, b = -3.0, c = -4.0;
 
-            Console.WriteLine(resulta);
+            //double delta = b * b - 4.0 * a * c;
+            double delta = Math.Pow(b, 2.0) - 4.0 * a * c;
+
+            double x1 = (-b + Math.Sqrt(delta)) / (2.0 * a);
+            double x2 = (-b - Math.Sqrt(delta)) / (2.0 * a);
+
+            Console.WriteLine(n1);
+            Console.WriteLine(n2);
+            Console.WriteLine(n3);
+            Console.WriteLine(n4);
+            Console.WriteLine(delta);
+            Console.WriteLine(x1);
+            Console.WriteLine(x2);
 
         }
     }
