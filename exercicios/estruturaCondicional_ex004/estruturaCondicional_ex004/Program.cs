acd6e206811a909c319a887a29b3098c2ffd223e
devpablohenrique <devpablohenrique@gmail.com>
2022-06-01 16:53:19 -0300

    Exercicio condicional 04

---
+++ exercicios/estruturaCondicional_ex004/estruturaCondicional_ex004/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace estruturaCondicional_ex004
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Informe a hora inicial e a hora final do jogo: ");
+            string[] vet = Console.ReadLine().Split();
+
+            int inicial = int.Parse(vet[0]);
+            int final = int.Parse(vet[1]);
+
+            if (inicial >= final)
+            {
+                Console.WriteLine($"O JOGO DUROU {24 - inicial + final} HORA(S)");
+            }
+            else
+            {
+                Console.WriteLine($"O JOGO DUROU { final - inicial} HORA(S)");
+            }
+
+        }
+    }
+}
