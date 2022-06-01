a25ee2759df8235df43ed6c1eb1e4ba2b800f16c
devpablohenrique <devpablohenrique@gmail.com>
2022-06-01 16:01:13 -0300

    Exercicio condicional 03

---
+++ exercicios/estruturaCondicional_ex003/estruturaCondicional_ex003/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace estruturaCondicional_ex003
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Informe dois valores inteiro: ");
+            string[] vet = Console.ReadLine().Split(' ');
+
+            int a = int.Parse(vet[0]);
+            int b = int.Parse(vet[1]);
+
+            if ( a > b)
+            {
+                if (a % b == 0)
+                {
+                    Console.WriteLine("São Multiplos");
+                }
+                else
+                {
+                    Console.WriteLine("Não são Multiplos");
+                }
+            }
+            else
+            {
+                if (b % a == 0)
+                {
+                    Console.WriteLine("São Multiplos");
+                }
+                else
+                {
+                    Console.WriteLine("Não são Multiplos");
+                }
+            }
+            
+        }
+    }
+}
