2a27956467b08b773726de6ee928ccfb9ed1c80d
devpablohenrique <devpablohenrique@gmail.com>
2022-06-01 17:47:15 -0300

    Exercicio condicional 07

---
+++ exercicios/estruturaCondicional_ex006/estruturaCondicional_ex006/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Globalization;
+
+namespace estruturaCondicional_ex006
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Informe um valor real de 0 a 100: ");
+            double val = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            if (val < 0 || val > 100)
+            {
+                Console.WriteLine("Fora do intervalo");
+            }
+            else if (val <= 25 )
+            {
+                Console.WriteLine("Intervalo (0, 25)");
+            }
+            else if (val <= 50)
+            {
+                Console.WriteLine("Intervalo (25, 50)");
+            }
+            else if (val <= 75)
+            {
+                Console.WriteLine("Intervalo (50, 75)");
+            }
+            else
+            {
+                Console.WriteLine("Intervalo (75, 100)");
+            }
+        }
+    }
+}
---
+++ exercicios/estruturaCondicional_ex007/estruturaCondicional_ex007/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Globalization;
+
+namespace estruturaCondicional_ex007
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Insira dois valores REAIS: ");
+            string[] vet = Console.ReadLine().Split(' ');
+
+            double x = double.Parse(vet[0], CultureInfo.InvariantCulture);
+            double y = double.Parse(vet[1], CultureInfo.InvariantCulture);
+
+            if (x == 0 && y == 0)
+            {
+                Console.WriteLine("Origem");
+            }
+            else if ( x == 0 )
+            {
+                Console.WriteLine("Eixo X");
+            }
+            else if ( y == 0)
+            {
+                Console.WriteLine("Eito Y");
+            }
+            else if (x > 0 && y > 0)
+            {
+                Console.WriteLine("Q1");
+            }
+            else if ( x > 0 && y < 0)
+            {
+                Console.WriteLine("Q4");
+            }
+            else if ( x < 0 && y > 0)
+            {
+                Console.WriteLine("Q2");
+            }
+            else
+            {
+                Console.WriteLine("Q3");
+            }
+        }
+    }
+}
