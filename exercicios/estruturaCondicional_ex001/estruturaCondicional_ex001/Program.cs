714336cd2d2bf8b9c7f10991a28d6075fbae2afb
devpablohenrique <devpablohenrique@gmail.com>
2022-06-01 15:47:13 -0300

    Exercicio condicional 01

---
+++ exercicios/estruturaCondicional_ex001/estruturaCondicional_ex001/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace estruturaCondicional_ex001
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Informe um numero inteiro para saber se é positivo ou negativo: ");
+            int num = int.Parse(Console.ReadLine());
+
+            if ( num < 0)
+            {
+                Console.WriteLine($"O numero informado {num} é NEGATIVO");
+            }
+            else
+            {
+                Console.WriteLine($"O numero informado {num} é POSITIVO");
+            }
+        }
+    }
+}
