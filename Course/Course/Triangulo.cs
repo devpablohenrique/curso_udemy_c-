bf2b7ff45dc70b92d9b430773b255e9cf8e5847a
devpablohenrique <devpablohenrique@gmail.com>
2022-06-02 14:59:31 -0300

    Criando metodos na classe triangulo

--- Course/Course/Triangulo.cs
+++ Course/Course/Triangulo.cs
@@ -1,10 +1,18 @@
-﻿namespace Course
+﻿using System;
+
+namespace Course
 {
     internal class Triangulo
     {
         public double A;
         public double B;
         public double C;
+
+        public double Area()
+        {
+            double p = (A + B + C) / 2;
+            return Math.Sqrt(p * (p - A) * (p - B) * (p - C));
+        }
 
     }
 }
