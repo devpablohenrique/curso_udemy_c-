7474b927ee68af1879e2a619cafb57985f5ebc4c
devpablohenrique <devpablohenrique@gmail.com>
2022-06-02 17:06:06 -0300

    Objetct e ToString

---
+++ Course/Course/Produto.cs
@@ -0,0 +1,27 @@
+﻿using System.Globalization;
+
+namespace Course
+{
+    internal class Produto
+    {
+        public string Nome;
+        public double Preco;
+        public int Quantidade;
+
+        public double ValorTotalEmEstoque()
+        {
+            return Preco * Quantidade;
+        }
+
+        public override string ToString()
+        {
+            return Nome
+                + ", $ " +
+                Preco.ToString("F2", CultureInfo.InvariantCulture)
+                + ", "
+                + Quantidade
+                + " unidades, Total: $ "
+                + ValorTotalEmEstoque().ToString("F2", CultureInfo.InvariantCulture);
+        }
+    }
+}
--- Course/Course/Program.cs
+++ Course/Course/Program.cs
@@ -7,36 +7,18 @@ namespace Course
     {
         static void Main(string[] args)
         {
-            Triangulo x, y;
+            Produto p = new Produto();
 
-            x = new Triangulo();
-            y = new Triangulo();
+            Console.WriteLine("Entre os dados do produto: ");
+            Console.Write("Nome: ");
+            p.Nome = Console.ReadLine();
+            Console.Write("Preco: ");
+            p.Preco = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.Write("Quantidade no estoque: ");
+            p.Quantidade = int.Parse(Console.ReadLine());
 
-            Console.WriteLine("Entre com as medidas do triangulo x: ");
-            x.A = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
-            x.B = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
-            x.C = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
 
-            Console.WriteLine("Entre com as medidas do triangulo y: ");
-            y.A = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
-            y.B = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
-            y.C = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
-
-            double areaX = x.Area();
-            double areaY = y.Area();
-
-            Console.WriteLine("Área de x = " + areaX.ToString("F4", CultureInfo.InvariantCulture));
-            Console.WriteLine("Área de y = " + areaY.ToString("F4", CultureInfo.InvariantCulture));
-
-            if (areaX > areaY)
-            {
-                Console.WriteLine("Maior área: X");
-            }
-            else
-            {
-                Console.WriteLine("Maior área: Y");
-            }
-
+            Console.WriteLine("Dados do produto: "+ p);
         }
     }
 }
