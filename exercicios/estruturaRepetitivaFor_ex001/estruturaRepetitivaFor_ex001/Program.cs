14637113b059a899aa643bc152bb2f7fe072ba36
devpablohenrique <devpablohenrique@gmail.com>
2022-06-02 07:55:23 -0300

    Exercicio For 001

---
+++ exercicios/estruturaRepetitivaFor_ex001/estruturaRepetitivaFor_ex001/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace estruturaRepetitivaFor_ex001
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Informe um valor inteiro, onde seja maior que 0 e menor/igual a 1000: ");
+            int x = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= x; i++)
+            {
+                if ( i % 2 != 0)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+        }
+    }
+}
