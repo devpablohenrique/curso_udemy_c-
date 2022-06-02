d209a5c4062f06aace23bba89869a469ebe0c232
devpablohenrique <devpablohenrique@gmail.com>
2022-06-02 09:40:30 -0300

    Exercicio For 007

---
+++ exercicios/estruturaRepetitivaFor_ex007/estruturaRepetitivaFor_ex007/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace estruturaRepetitivaFor_ex007
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = int.Parse(Console.ReadLine());
+
+            for ( int i = 1; i <= num; i++)
+            {
+                Console.WriteLine($"{i} {Math.Pow(i, 2)} {Math.Pow(i,3)}");
+            }
+        }
+    }
+}
