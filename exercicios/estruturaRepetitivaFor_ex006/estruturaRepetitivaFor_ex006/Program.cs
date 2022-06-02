8179aa4447a031c7075e30b48676313ee49c85d1
devpablohenrique <devpablohenrique@gmail.com>
2022-06-02 09:37:05 -0300

    Exercicio For 006

---
+++ exercicios/estruturaRepetitivaFor_ex006/estruturaRepetitivaFor_ex006/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace estruturaRepetitivaFor_ex006
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = int.Parse(Console.ReadLine());
+
+            for ( int i = 1; i <= num; i++)
+            {
+                if ( num % i == 0)
+                {
+                    Console.WriteLine(i);
+                }           
+            }
+        }
+    }
+}
