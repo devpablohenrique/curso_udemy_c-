3907cbe6139e167dafb3cba8a69f74ef7cb20510
devpablohenrique <devpablohenrique@gmail.com>
2022-06-01 22:32:11 -0300

    Exercicio while 01

---
+++ exercicios/estruturaRepeticao_ex001/estruturaRepeticao_ex001/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace estruturaRepeticao_ex001
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Informe a senha: ");
+            string senha = Console.ReadLine();
+
+            while (senha != "2002")
+            {
+                Console.Write("Senha Invalida, tente novamente: ");
+                senha = Console.ReadLine();
+            }
+            Console.WriteLine("Acesso Permitido!");
+        }
+    }
+}
