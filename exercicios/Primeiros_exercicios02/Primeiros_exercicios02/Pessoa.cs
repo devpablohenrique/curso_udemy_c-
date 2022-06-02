91260c262ff10428c5b2757fa6975ea7d83a7255
devpablohenrique <devpablohenrique@gmail.com>
2022-06-02 13:20:22 -0300

    Ex. primeiros passos 02

---
+++ exercicios/Primeiros_exercicios02/Primeiros_exercicios02/Pessoa.cs
@@ -0,0 +1,8 @@
+﻿namespace Primeiros_exercicios02
+{
+    internal class Pessoa
+    {
+        public string Nome;
+        public double Salario;
+    }
+}
---
+++ exercicios/Primeiros_exercicios02/Primeiros_exercicios02/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Globalization;
+
+namespace Primeiros_exercicios02
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Pessoa p1, p2;
+
+            p1 = new Pessoa();
+            p2 = new Pessoa();
+
+            Console.WriteLine("Dados do primeiro funcionário: ");
+            Console.Write("Nome: ");
+            p1.Nome = Console.ReadLine();
+            Console.Write("Salário: ");
+            p1.Salario = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.WriteLine("Dados do segundo funcionário: ");
+            Console.Write("Nome: ");
+            p2.Nome = Console.ReadLine();
+            Console.Write("Salário: ");
+            p2.Salario = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            double mediaSal = (p1.Salario + p2.Salario) / 2;
+
+            Console.WriteLine("Salário médio = {0}", mediaSal.ToString("F2", CultureInfo.InvariantCulture));
+
+        }
+    }
+}
