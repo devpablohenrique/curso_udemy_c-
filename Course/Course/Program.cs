e8c99a21957afb61e76e48503ef8d336384eae4a
devpablohenrique <devpablohenrique@gmail.com>
2022-06-02 07:48:41 -0300

    Estrutura repetitiva for

--- Course/Course/Program.cs
+++ Course/Course/Program.cs
@@ -7,17 +7,19 @@ namespace Course
     {
         static void Main(string[] args)
         {
-            Console.Write("Digite um número: ");
-            double x = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.Write("Quantos números inteiros você vai digitar? ");
+            int n = int.Parse(Console.ReadLine());
 
-            while ( x >= 0)
+            int soma = 0;
+
+            for ( int i = 1; i <= n; i++)
             {
-                double raiz = Math.Sqrt(x);
-                Console.WriteLine(raiz.ToString("F3", CultureInfo.InvariantCulture));
-                Console.Write("Digite outro número: ");
-                x = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                Console.Write("Valor #{0}: ", i);
+                int valor = int.Parse(Console.ReadLine());
+                soma += valor;
             }
-            Console.WriteLine("Número negativo!");
+
+            Console.WriteLine("Soma = {0}", soma);
             
         }
     }
