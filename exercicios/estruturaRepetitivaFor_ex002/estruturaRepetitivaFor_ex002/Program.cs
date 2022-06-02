fa65d99fc628c1bb385614c951b21bf2a6eab635
devpablohenrique <devpablohenrique@gmail.com>
2022-06-02 08:06:02 -0300

    Exercicio For 002

---
+++ exercicios/estruturaRepetitivaFor_ex002/estruturaRepetitivaFor_ex002/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace estruturaRepetitivaFor_ex002
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Informe quantos valores serão lidos: ");
+            int n = int.Parse(Console.ReadLine());
+
+            int valIn = 0;
+            int valOut = 0;
+
+            for (int i = 1; i <= n; i++)
+            {
+                Console.Write("Informe o valor #{0}: ", i);
+                int val = int.Parse(Console.ReadLine());
+
+                if ( val >= 10 && val <= 20)
+                {
+                    valIn++;
+                }
+                else
+                {
+                    valOut++;
+                }
+            }
+            Console.WriteLine($"{valIn} in");
+            Console.WriteLine($"{valOut} out");
+        }
+    }
+}
