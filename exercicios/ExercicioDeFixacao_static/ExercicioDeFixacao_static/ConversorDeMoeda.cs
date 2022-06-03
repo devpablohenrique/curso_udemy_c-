2dc99f0bcca4af2bcb896d4c5bd600dd92b4e390
devpablohenrique <devpablohenrique@gmail.com>
2022-06-03 14:55:56 -0300

    Finalizando a secao 4

---
+++ exercicios/ExercicioDeFixacao_static/ExercicioDeFixacao_static/ConversorDeMoeda.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace ExercicioDeFixacao_static
+{
+    internal class ConversorDeMoeda
+    {
+        public static double Iof = 0.06;
+
+        public static double RealParaDolar(double cotacao, double qtdDolar)
+        {
+            double saldoParcial = qtdDolar * cotacao;
+
+            return saldoParcial += saldoParcial * Iof;
+        }
+    }
+}
