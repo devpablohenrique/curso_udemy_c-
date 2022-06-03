dc397e4ed235fe2dddf116eaef943f124e9eb1f1
devpablohenrique <devpablohenrique@gmail.com>
2022-06-02 22:14:23 -0300

    Resolução exercicio

---
+++ exercicios/ExClasseAtribMetod001/ExClasseAtribMetod001/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Globalization;
+
+namespace ExClasseAtribMetod001
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Retangulo retangulo = new Retangulo();
+
+            Console.WriteLine("Entre a largura e altura do retângulo: ");
+            retangulo.Altura = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            retangulo.Largura = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.WriteLine("AREA = {0}", retangulo.Area().ToString("F2", CultureInfo.InvariantCulture));
+            Console.WriteLine("PERÍMETRO = {0}", retangulo.Perimetro().ToString("F2", CultureInfo.InvariantCulture));
+            Console.WriteLine("DIAGONAL = {0}", retangulo.Diagonal().ToString("F2", CultureInfo.InvariantCulture));
+        }
+    }
+}
