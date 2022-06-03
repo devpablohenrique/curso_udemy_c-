e7187788c5f189afd8ede0eab91d9726f46108be
devpablohenrique <devpablohenrique@gmail.com>
2022-06-03 15:25:55 -0300

    Construtores

--- Course/Course/Program.cs
+++ Course/Course/Program.cs
@@ -3,19 +3,41 @@ using System.Globalization;
 
 namespace Course
 {
-    internal class Program
+    class Program
     {
         static void Main(string[] args)
         {
-            Console.Write("Entre o valor do raio: ");
-            double raio = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.WriteLine("Entre os dados do produto:");
+            Console.Write("Nome: ");
+            string nome = Console.ReadLine();
+            Console.Write("Preço: ");
+            double preco = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.Write("Quantidade no estoque: ");
+            int quantidade = int.Parse(Console.ReadLine());
 
-            double circ = Calculadora.Circuferencia(raio);
-            double volume = Calculadora.Volume(raio);
+            Produto p = new Produto(nome, preco, quantidade);
 
-            Console.WriteLine("Circuferencia : " + circ.ToString("F2", CultureInfo.InvariantCulture));
-            Console.WriteLine("Volume: " + volume.ToString("F2", CultureInfo.InvariantCulture));
-            Console.WriteLine("Valor de PI: " + Calculadora.Pi.ToString("F2", CultureInfo.InvariantCulture));
+            Console.WriteLine();
+            Console.WriteLine("Dados do produto: " + p);
+
+            Console.WriteLine();
+            Console.Write("Digite o número de produtos a ser adicionado ao estoque: ");
+            int qte = int.Parse(Console.ReadLine());
+
+            p.AdicionarProdutos(qte);
+
+            Console.WriteLine();
+            Console.WriteLine("Dados atualizados: " + p);
+
+            Console.WriteLine();
+            Console.Write("Digite o número de produtos a ser removido do estoque: ");
+            qte = int.Parse(Console.ReadLine());
+
+            p.RemoverProdutos(qte);
+
+            Console.WriteLine();
+            Console.WriteLine("Dados atualizados: " + p);
+            
         }
     }
 }
