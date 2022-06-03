7721182d14f410a5eef396e4a8005c0ff8027121
devpablohenrique <devpablohenrique@gmail.com>
2022-06-02 22:58:38 -0300

    Resolução exercicio03

--- exercicios/ExClasseAtribMetod003/ExClasseAtribMetod003/Aluno.cs
+++ exercicios/ExClasseAtribMetod003/ExClasseAtribMetod003/Aluno.cs
@@ -1,12 +1,29 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Globalization;
 
 namespace ExClasseAtribMetod003
 {
     internal class Aluno
     {
+        public string Nome;
+        public double[] Nota = new double[3];
+
+        public double NotaFinal()
+        {
+            return Nota[0] + Nota[1] + Nota[2];
+        }
+
+        public void Aprovacao()
+        {
+            if ( NotaFinal() >= 60.00 )
+            {
+                Console.WriteLine("APROVADO");
+            }
+            else
+            {
+                Console.WriteLine("REPROVADO");
+                Console.WriteLine("FALTARAM {0} PONTOS", (60.00 - NotaFinal()).ToString("F2", CultureInfo.InvariantCulture));
+            }
+        }
     }
 }
--- exercicios/ExClasseAtribMetod003/ExClasseAtribMetod003/Program.cs
+++ exercicios/ExClasseAtribMetod003/ExClasseAtribMetod003/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Globalization;
 
 namespace ExClasseAtribMetod003
 {
@@ -6,7 +7,18 @@ namespace ExClasseAtribMetod003
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Aluno aluno = new Aluno();
+
+            Console.Write("Nome do Aluno: ");
+            aluno.Nome = Console.ReadLine();
+            Console.WriteLine("Digite as três notas do aluno: ");
+            aluno.Nota[0] = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            aluno.Nota[1] = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            aluno.Nota[2] = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.WriteLine("NOTA FINAL = "+ aluno.NotaFinal().ToString("F2", CultureInfo.InvariantCulture));
+            aluno.Aprovacao();
+
         }
     }
 }
