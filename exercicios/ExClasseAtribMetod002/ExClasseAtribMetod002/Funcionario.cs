dc97e975a302205c8352a1c70662ee4635dbb1a5
devpablohenrique <devpablohenrique@gmail.com>
2022-06-02 22:43:44 -0300

    Resolução exercicio02

---
+++ exercicios/ExClasseAtribMetod002/ExClasseAtribMetod002/Funcionario.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Globalization;
+
+namespace ExClasseAtribMetod002
+{
+    internal class Funcionario
+    {
+        public string Nome;
+        public double SalarioBruto;
+        public double Imposto;
+
+        public double SalarioLiquido()
+        {
+            return SalarioBruto - Imposto;
+        }
+
+        public void AumentarSalario(double porcentagem)
+        {
+            SalarioBruto += SalarioBruto * (porcentagem / 100);
+        }
+
+        public override string ToString()
+        {
+            return Nome + ", $ " + SalarioLiquido().ToString("F2", CultureInfo.InvariantCulture);
+        }
+
+    }
+}
---
+++ exercicios/ExClasseAtribMetod003/ExClasseAtribMetod003/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ExClasseAtribMetod003
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
