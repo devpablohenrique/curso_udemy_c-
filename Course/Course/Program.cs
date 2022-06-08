09e16108bb57c30aa50dc66c47d4e8b686ff18dd
devpablohenrique <devpablohenrique@gmail.com>
2022-06-08 10:07:01 -0300

    Modificador de parametros params

--- Course/Course/Program.cs
+++ Course/Course/Program.cs
@@ -8,28 +8,11 @@ namespace Course
         static void Main(string[] args)
         {
 
-            int n = int.Parse(Console.ReadLine());
+            int s1 = Calculator.Sum( 2, 3 );
+            int s2 = Calculator.Sum( 2, 4, 3 );
 
-            Product[] vect = new Product[n];
-
-            for ( int i = 0; i < n; i++)
-            {
-                string name = Console.ReadLine();
-                double price = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
-
-                vect[i] = new Product { Name = name, Price = price };
-            }
-
-            double sum = 0.0;
-
-            for ( int i = 0; i < n; i++)
-            {
-                sum += vect[i].Price;
-            }
-
-            double avg = sum / n;
-
-            Console.WriteLine("AVERAGE PRICE = "+ avg.ToString("F2", CultureInfo.InvariantCulture));
+            Console.WriteLine(s1);
+            Console.WriteLine(s2);
         }
     }
 }
