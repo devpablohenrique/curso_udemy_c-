16f18228fd33274a36bb71616c49177da42c1d3d
devpablohenrique <devpablohenrique@gmail.com>
2022-06-09 14:22:06 -0300

    Listas

--- Course/Course/Product.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Course
-{
-    internal class Product
-    {
-        public string Name { get; set; }
-        public double Price { get; set; }
-    }
-}
--- Course/Course/Program.cs
+++ Course/Course/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Globalization;
+using System.Collections.Generic;
 
 namespace Course
 {
@@ -8,11 +9,61 @@ namespace Course
         static void Main(string[] args)
         {
 
-            int a = 10;
-            int triple;
+            List<string> list = new List<string>();
 
-            Calculator.Triple(a, out triple);
-            Console.WriteLine(triple);
+            list.Add("Maria");
+            list.Add("Alex");
+            list.Add("Bob");
+            list.Add("Anna");
+
+            list.Insert(2, "Marco");
+
+            foreach (string obj in list)
+            {
+                Console.WriteLine(obj);
+            }
+            Console.WriteLine("List count: " + list.Count);
+
+            string s1 = list.Find(x => x[0] == 'A');
+            Console.WriteLine("First 'A': " + s1);
+
+            string s2 = list.FindLast(x => x[0] == 'A');
+            Console.WriteLine("Last 'A': " + s2);
+
+            int pos1 = list.FindIndex(x => x[0] == 'A');
+            Console.WriteLine("First position: 'A': " + pos1);
+
+            int pos2 = list.FindLastIndex(x => x[0] == 'A');
+            Console.WriteLine("Last position: 'A': " + pos2);
+
+            List<string> list2 = list.FindAll(x => x.Length == 5);
+            Console.WriteLine("-------------------------------------");
+
+            foreach (string obj in list2)
+            {
+                Console.WriteLine(obj);
+            }
+
+            //list.Remove("Alex");
+            //list.RemoveAt(3);
+            list.RemoveRange(2, 2);
+            Console.WriteLine("-------------------------------------");
+
+            foreach (string obj in list)
+            {
+                Console.WriteLine(obj);
+            }
+            Console.WriteLine("-------------------------------------");
+
+            list.RemoveAll(x => x[0] == 'M');
+            foreach (string obj in list)
+            {
+                Console.WriteLine(obj);
+            }
+            Console.WriteLine("-------------------------------------");
+
+
+
         }
     }
 }
