c135ab292ea65ddf4f09d6fe2545b201dd59619d
pablo_desenv <devpablohenrique@gmail.com>
2022-08-04 13:45:37 -0300

    Aula 79

--- Course/Course/Program.cs
+++ Course/Course/Program.cs
@@ -8,62 +8,15 @@ namespace Course
     {
         static void Main(string[] args)
         {
+            double[,] mat = new double[2, 3];
 
-            List<string> list = new List<string>();
+            Console.WriteLine(mat.Length);
 
-            list.Add("Maria");
-            list.Add("Alex");
-            list.Add("Bob");
-            list.Add("Anna");
+            Console.WriteLine(mat.Rank);
 
-            list.Insert(2, "Marco");
+            Console.WriteLine(mat.GetLength(0));
 
-            foreach (string obj in list)
-            {
-                Console.WriteLine(obj);
-            }
-            Console.WriteLine("List count: " + list.Count);
-
-            string s1 = list.Find(x => x[0] == 'A');
-            Console.WriteLine("First 'A': " + s1);
-
-            string s2 = list.FindLast(x => x[0] == 'A');
-            Console.WriteLine("Last 'A': " + s2);
-
-            int pos1 = list.FindIndex(x => x[0] == 'A');
-            Console.WriteLine("First position: 'A': " + pos1);
-
-            int pos2 = list.FindLastIndex(x => x[0] == 'A');
-            Console.WriteLine("Last position: 'A': " + pos2);
-
-            List<string> list2 = list.FindAll(x => x.Length == 5);
-            Console.WriteLine("-------------------------------------");
-
-            foreach (string obj in list2)
-            {
-                Console.WriteLine(obj);
-            }
-
-            //list.Remove("Alex");
-            //list.RemoveAt(3);
-            list.RemoveRange(2, 2);
-            Console.WriteLine("-------------------------------------");
-
-            foreach (string obj in list)
-            {
-                Console.WriteLine(obj);
-            }
-            Console.WriteLine("-------------------------------------");
-
-            list.RemoveAll(x => x[0] == 'M');
-            foreach (string obj in list)
-            {
-                Console.WriteLine(obj);
-            }
-            Console.WriteLine("-------------------------------------");
-
-
-
+            Console.WriteLine(mat.GetLength(1));
         }
     }
 }
