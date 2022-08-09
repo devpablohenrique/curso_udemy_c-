4989dfa4f6645832fe9dd792cd320cc1343de0fa
pablo_desenv <devpablohenrique@gmail.com>
2022-08-09 20:54:51 -0300

    var

--- Course/Course/Program.cs
+++ Course/Course/Program.cs
@@ -8,15 +8,40 @@ namespace Course
     {
         static void Main(string[] args)
         {
-            double[,] mat = new double[2, 3];
+            int n = int.Parse(Console.ReadLine());
 
-            Console.WriteLine(mat.Length);
+            int[,] mat = new int[n, n];
 
-            Console.WriteLine(mat.Rank);
+            for ( int i = 0; i < n; i++)
+            {
+                string[] values = Console.ReadLine().Split(' ');
 
-            Console.WriteLine(mat.GetLength(0));
+                for ( int j = 0; j < n; j++)
+                {
+                    mat[i, j] = int.Parse(values[j]);
+                }
+            }
 
-            Console.WriteLine(mat.GetLength(1));
+            Console.WriteLine("Main diagonal: ");
+            for ( int i = 0; i < n; i++)
+            {
+                Console.Write(mat[i,i] + " ");
+            }
+            Console.WriteLine();
+
+            int count = 0;
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    if (mat[i, j] < 0)
+                    {
+                        count++;
+                    }
+                }
+            }
+
+            Console.WriteLine("Negative numbers: "+ count);
         }
     }
 }
---
+++ exercicios/exercicio_fixacao_matrizes/exercicio_fixacao_matrizes/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace exercicio_fixacao_matrizes
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var x = 10;
+            var y = 20.0;
+            var z = "Maria";
+
+            int w = z;
+
+            Console.WriteLine(x);
+            Console.WriteLine(y);
+            Console.WriteLine(z);
+
+
+        }
+    }
+}
