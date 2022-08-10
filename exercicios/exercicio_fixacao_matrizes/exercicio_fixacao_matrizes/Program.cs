aa6c49c789471eba9c54565f6e5a2a88f8d26567
pablo_desenv <devpablohenrique@gmail.com>
2022-08-09 21:04:14 -0300

    switch-case

--- exercicios/exercicio_fixacao_matrizes/exercicio_fixacao_matrizes/Program.cs
+++ exercicios/exercicio_fixacao_matrizes/exercicio_fixacao_matrizes/Program.cs
@@ -6,15 +6,38 @@ namespace exercicio_fixacao_matrizes
     {
         static void Main(string[] args)
         {
-            var x = 10;
-            var y = 20.0;
-            var z = "Maria";
 
-            int w = z;
+            int x = int.Parse(Console.ReadLine());
+            string day;
 
-            Console.WriteLine(x);
-            Console.WriteLine(y);
-            Console.WriteLine(z);
+            switch (x)
+            {
+                case 1:
+                    day = "Sunday";
+                    break;
+                case 2:
+                    day = "Monday";
+                    break;
+                case 3:
+                    day = "Tuesday";
+                    break;
+                case 4:
+                    day = "Wednesday";
+                    break;
+                case 5:
+                    day = "Thursday";
+                    break;
+                case 6:
+                    day = "Friday";
+                    break;
+                case 7:
+                    day = "Saturday";
+                    break;
+                default:
+                    day = "Invalid value!";
+                    break;
+            }
+            Console.WriteLine("Day: "+ day);
 
 
         }
